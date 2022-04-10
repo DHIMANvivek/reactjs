02e24b5e5e776c765f63d798c88a1068e13e41c0
DHIMANvivek <googlydhiman.4236@gmail.com>
2022-04-10 17:22:19 +0530

    react

---
+++ src/component/Basics/MenuCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Menu from "./menuApi";
+const MenuCard = ({ menuData }) => {
+  const myStyle = { color: "red" };
+  return (
+    <>
+      <section className="main-card--cointainer">
+        {menuData.map((item) => {
+          return (
+            <>
+              <div className="card-container" key={item.id}>
+                <div className="card">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle">
+                      {item.id}
+                    </span>
+                    <span className="card-author subtle" style={myStyle}>
+                      {item.category}
+                    </span>
+                    <h2 className="card-title">{item.name}</h2>
+                    <span className="card-description subtle">
+                      {item.description}
+                    </span>
+                    <div className="card-read">Read</div>
+                  </div>
+                  <img src={item.image} alt="images" className="card-media" />
+
+                  <span className="card-tag subtle">Order Now</span>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default MenuCard;
---
+++ src/component/Basics/Restaurant.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+import Menu from "./menuApi";
+import { useState } from "react";
+import MenuCard from "./MenuCard";
+
+
+const Restaurant = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const filterItem = (category) => {
+    const updatedList = Menu.filter((item) => {
+      return item.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+  return (
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          <button className="btn-group__item" onClick={() => filterItem("breakfast")}>
+            BreakFast
+          </button>
+          <button className="btn-group__item" onClick={() => filterItem("lunch")}>Lunch</button>
+          <button className="btn-group__item" onClick={() => filterItem("evening")}>Evening</button>
+          <button className="btn-group__item" onClick={() => filterItem("dinner")}>Dinner</button>
+          <button className="btn-group__item" onClick={() => setMenuData(Menu)}>All</button>
+        </div>
+      </nav>
+      <MenuCard menuData={menuData} />
+    </>
+  );
+};
+
+export default Restaurant;
